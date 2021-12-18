88b113f54662d5ef000d7df22786cfba09f3a760
d-fuqua <davidfuqua1627@gmail.com>
2021-12-18 00:03:56 -0500

    Final Server Changes

--- server/server.js
+++ server/server.js
@@ -35,6 +35,113 @@ app.post('/create', (req, res) => {
     );
 });
 
+app.get('/portfolios', (req, res) => {
+    db.query(
+        'SELECT * FROM Portfolio',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    );
+});
+
+app.get('/max_asset', (req, res) => {
+    db.query(
+        'call Mercury.GetAssetMax()',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    );
+});
+
+app.post('/create-budget-goal', (req, res) => {
+    const Objective = req.body.objective
+    const Timeframe = req.body.timeframe
+    const Projected_Expenses = req.body.projectedExpenses
+    const Projected_Income = req.body.projectedIncome
+    const Portfolio_Id = req.body.portfolioId
+
+    db.query(
+        'INSERT INTO Budget_Goals (Objective, Timeframe, Projected_Expenses, Projected_Income, Portfolio_ID) VALUES (?,?,?,?,?)',
+        [Objective, Timeframe, Projected_Expenses, Projected_Income, Portfolio_Id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("Values Inserted")
+            }
+        }
+    );
+});
+
+app.get('/portfolio-budget-goal', (req, res) => {
+    db.query(
+        'SELECT bg.Portfolio_ID as PortID, p.Account_Name as AccName, bg.Objective as Obj, bg.Timeframe as Timeframe, p.Asset_Total as AstTot FROM Budget_Goals as bg LEFT OUTER JOIN Portfolio as p ON bg.Portfolio_ID=p.Portfolio_ID ORDER BY PortID',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    );
+});
+
+app.get('/timeframe-averages', (req, res) => {
+    db.query(
+        'call Mercury.getBudgetAvgs()',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    );
+});
+
+app.post('/create-transaction', (req, res) => {
+    const TxNum = req.body.txNum
+    const Account_Name = req.body.accountName
+    const Amount = req.body.amount
+    const Transaction_Type = req.body.transactionType
+    const Vender = req.body.vender
+    const Earnings = req.body.earning
+    const Account_ID = req.body.accountId
+
+    db.query(
+        'INSERT INTO Transactions (TxNum, Account_Name, Amount, Transaction_Type, Vender, Earnings, Account_ID) VALUES (?,?,?,?,?,?,?)',
+        [TxNum, Account_Name, Amount, Transaction_Type, Vender, Earnings, Account_ID],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("Values Inserted")
+            }
+        }
+    );
+});
+
+app.get('/total-earnings', (req, res) => {
+    db.query(
+        'call Mercury.getAccountTotal()',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    );
+});
+
 app.listen(3001, () => {
-    console.log("Yay, your server is running on port 3001")
+    console.log("Your server is running on port 3001")
 })
